1309c1765b27e37b9bdf3e4016d6736f7f568a58
junyi5257 <junyi5257@163.com>
2017-04-11 11:17:01 +0800

    AAAAAAAAAAAA

---
+++ src/main/java/cn/app/zookeeper/service/DemoService.java
@@ -0,0 +1,14 @@
+package cn.app.zookeeper.service;
+
+/**
+ * Created by goujy on 2017/3/29.
+ */
+public interface DemoService {
+    /**
+     * 测试方法
+     *
+     * @param name 参数name
+     * @return 返回字符串
+     */
+    String sayHello(String name);
+}
---
+++ src/main/java/cn/app/zookeeper/service/impl/Consumer.java
@@ -0,0 +1,26 @@
+package cn.app.zookeeper.service.impl;
+
+import cn.app.zookeeper.service.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 测试客户端
+ * Created by goujy on 2017/3/30.
+ */
+public class Consumer {
+
+    public static void main(String[] args) {
+        System.out.println("Start to load--Consumer");
+        //
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"application-context.xml"});
+        context.start();
+        //
+        System.out.println("End to load--Consumer");
+
+        DemoService demoService = (DemoService) context.getBean("demoService");
+        String result = demoService.sayHello("World");
+        System.out.println("result of dubbo: " + result);
+
+
+    }
+}
